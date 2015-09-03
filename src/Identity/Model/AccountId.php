750eb04e628fa3629d60d7ebd42da6a39b799b26
mitchellolsthoorn <mitchell.olsthoorn@outlook.com>
2015-09-03 23:09:59 +0200

    Added implementation for the identifier, to identify the account entity

---
+++ src/Identity/Model/AccountId.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace OG\Account\Domain\Identity\Model;
+
+use OG\Account\Domain\UuidIdentifier;
+
+/**
+ * The identifier value object to identify the account entity.
+ */
+class AccountId extends UuidIdentifier
+{
+}
