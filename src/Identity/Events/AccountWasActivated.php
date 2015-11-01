181672bada745df16ce0cc7f44e83718c8b32491
mitchellolsthoorn <mitchell.olsthoorn@outlook.com>
2015-11-01 13:58:47 +0100

    Added a method to account for activation

---
+++ src/Identity/Events/AccountWasActivated.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace OG\Account\Domain\Identity\Events;
+
+use OG\Core\Domain\DomainEvent;
+
+/**
+ * Event fired when an account was activated.
+ */
+class AccountWasActivated implements DomainEvent
+{
+    // TODO: Add extra information to the event.
+}
